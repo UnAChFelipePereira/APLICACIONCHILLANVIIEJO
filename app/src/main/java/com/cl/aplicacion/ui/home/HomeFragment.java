117a0e662f42beefab9870b6b059b21dd0408f05
Felipe Pereira S <Alexis.Saavedra@adventistas.org>
2023-06-14 18:45:38 -0400

    appChillan1

--- app/src/main/java/com/cl/aplicacion/ui/home/HomeFragment.java
+++ app/src/main/java/com/cl/aplicacion/ui/home/HomeFragment.java
@@ -11,40 +11,28 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.cl.aplicacion.R;
+import com.cl.aplicacion.databinding.FragmentHomeBinding;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-import com.cl.aplicacion.databinding.FragmentHomeBinding;
-
-public class HomeFragment extends Fragment implements OnMapReadyCallback{
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap googleMap;
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
-
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContent(R.layout.activity_main);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-        mapFragment.getMapAsync(this);
-    }
-
-
-
 
     @Override
     public void onDestroyView() {
@@ -52,12 +40,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         binding = null;
     }
 
-
     @Override
     public void onMapReady(GoogleMap map) {
         googleMap = map;
-        // Configura el mapa según tus necesidades
-        // Por ejemplo, puedes agregar marcadores, establecer la ubicación inicial, etc.
-    }
 
+    }
 }
